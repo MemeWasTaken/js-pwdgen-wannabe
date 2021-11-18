299511a9d888fa85345f842989b72d543a6d8261
MemeWasTaken <memelazzari02@gmail.com>
2021-11-18 15:39:30 +0100

    problem with words

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// New name variable
+    // Ask for user's name
+// New surname variable
+    // Ask for user's surname
+// New favColor variable
+    //Ask for user's favourite color
+// New lastDigitsYear constant
+
+// Print name+surname+favcolor+lastDigitYear
