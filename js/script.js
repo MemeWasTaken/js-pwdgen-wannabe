d82c57b0844f668dc1a8382c6b0341082d3fb5d7
MemeWasTaken <memelazzari02@gmail.com>
2021-11-18 16:06:49 +0100

    BONUS

--- js/script.js
+++ js/script.js
@@ -2,19 +2,23 @@
 let userName;
     // Ask for user's name
     userName = prompt('Inserisci il tuo nome:');
-    console.log(userName);
+    // console.log(userName);
 // New userSurname variable
 let userSurname;
     // Ask for user's surname
     userSurname = prompt('Inserisci il tuo cognome:');
-    console.log(userSurname);
+    // console.log(userSurname);
 // New favColor variable
 let favColor;
     //Ask for user's favourite color
-    favColor = prompt('Inserisci il tuo colore preferito');
-    console.log(favColor);
+    favColor = prompt('Inserisci il tuo colore preferito:');
+    // console.log(favColor);
     // New lastDigitsYear constant
 const lastDigitsYear = '21';
-console.log(lastDigitsYear);
+// console.log(lastDigitsYear);
 // Print name+surname+favcolor+lastDigitYear
-let psw = document.writeln(userName+userSurname+favColor+lastDigitsYear);
+// let psw = document.writeln(userName+userSurname+favColor+lastDigitsYear);
+
+let favNumber = parseInt(prompt('Inserisci il tuo numero preferito:'));
+let newNumber = favNumber + 5;
+let psw = document.writeln(userName+userSurname+favColor+newNumber);
