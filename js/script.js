a0c09eefeec73aabda630e5ac0ec4f8e4716330e
MemeWasTaken <memelazzari02@gmail.com>
2021-11-18 15:50:29 +0100

    userSurname

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@
 let userName;
     // Ask for user's name
     userName = prompt('Inserisci il tuo nome:');
-    console.log(userName)
+    console.log(userName);
 // New userSurname variable
+let userSurname;
     // Ask for user's surname
+    userSurname = prompt('Inserisci il tuo cognome:');
+    console.log(userSurname);
 // New favColor variable
     //Ask for user's favourite color
 // New lastDigitsYear constant
